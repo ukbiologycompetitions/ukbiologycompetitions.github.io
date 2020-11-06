6e99d3ea3b9edf13ad5512e9c176cc3bfcd73b2f
Matthew Johnston <mattjstn@outlook.com>
2020-11-06 00:12:40 +0000

    Contact forms

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -130,7 +130,7 @@
       data: data,
       timeout: 40000
     }).done( function(msg){
-      if (msg == 'OK') {
+      if (msg) {
         this_form.find('.loading').slideUp();
         this_form.find('.sent-message').slideDown();
         this_form.find("input:not(input[type=submit]), textarea").val('');
